35f6a1f209893232640edf1c87a1410415d2fe30
alejandro.samarin@gmail.com <alejandro.samarin@gmail.com@813cb5ac-386b-d9a5-ec4f-365354de57ff>
2011-05-09 13:49:48 +0000

    anyadido script creacion de usuarios y enunciado practica 3

---
+++ trunk/pract3/crear_usuarios_server.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function create_user {
+   useradd -m -d /export/casa/$1 -U -s /bin/bash -G $2 $1
+
+   # Cambiar la passwohrd del nuevo usuario (igual que su username)
+   echo $1:$1 | chpasswd
+
+   # Root sera el propietario de su directorio de conexion, para que el 
+   # usuario no pueda modificar los permisos del mismo
+   chown root /export/casa/$1
+
+   # Estableciendo permisos "rwxrwx---" se asegura que el usuario
+   # puede crear, modificar y borrar ficheros y directorios en su 
+   # 'home', a la vez que se previene que otros usuarios puedan acceder
+   chmod 770 /export/casa/$1
+}
+
+groupadd videojuegos
+groupadd portales
+
+mkdir -p /export/casa
+mkdir -p -m 0744 /export/comun
+mkdir -p -m 0700 /export/proyectos
+mkdir -m 2770 /export/proyectos/videojuegos
+chgrp videojuegos /export/proyectos/videojuegos
+mkdir -m 2770 /export/proyectos/portales
+chgrp portales /export/proyectos/portales
+
+setfacl -d -m g:videojuegos:rwx /export/proyectos/videojuegos
+setfacl -d -m g:portales:rwx /export/proyectos/acl
+
+create_user irene videojuegos
+create_user laura portales
+create_user clara videojuegos,portales
