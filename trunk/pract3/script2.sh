98ddd05526cf4167fd837a4bd8ecfb342d34e259
alejandrosamarin@gmail.com <alejandrosamarin@gmail.com@813cb5ac-386b-d9a5-ec4f-365354de57ff>
2011-05-10 16:03:36 +0000

    Anyadidos .ldif de la p3

--- trunk/pract3/script2.sh
+++ trunk/pract3/script2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ldapadd -x -D cn=Manager,dc=aso11,dc=org -H ldap://localhost -W -f ./passwds.ldif
-ldapadd -x -D cn=Manager,dc=aso11,dc=org -H ldap://localhost -W -f ./groups.ldif
+ldapadd -x -D cn=Manager,dc=aso11,dc=org -H ldap://localhost -W -f ./ous.ldif
+ldapadd -x -D cn=Manager,dc=aso11,dc=org -H ldap://localhost -W -f ./p3_passwds.ldif
+ldapadd -x -D cn=Manager,dc=aso11,dc=org -H ldap://localhost -W -f ./p3_groups.ldif
